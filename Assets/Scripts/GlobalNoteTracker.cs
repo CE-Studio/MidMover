6b83300b979eb4a735765310c90fb8fc8ad20af6
EpsilonTheDerg <jamesclamey@gmail.com>
2020-09-23 23:04:36 -0400

    no message

---
+++ Assets/Scripts/GlobalNoteTracker.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalNoteTracker : MonoBehaviour
+{
+    public static List<int> Track1 = new List<int>();
+    public float BPMinute;
+    public float BPMeasure;
+    public float timeUntilBeat;
+    public float timer;
+    public static int beat;
+    
+    void Start()
+    {
+        BPMinute = 128f;
+        BPMeasure = 8f;
+        Track1.Add(0); //
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0); //
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0); //
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0); //
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(15); //
+        Track1.Add(0);
+        Track1.Add(15);
+        Track1.Add(0);
+        Track1.Add(43);
+        Track1.Add(43);
+        Track1.Add(39);
+        Track1.Add(39);
+        Track1.Add(0); //
+        Track1.Add(0);
+        Track1.Add(15);
+        Track1.Add(0);
+        Track1.Add(43);
+        Track1.Add(43);
+        Track1.Add(39);
+        Track1.Add(39);
+        Track1.Add(0); //
+        Track1.Add(0);
+        Track1.Add(15);
+        Track1.Add(0);
+        Track1.Add(43);
+        Track1.Add(43);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(45); //
+        Track1.Add(45);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(46);
+        Track1.Add(46);
+        Track1.Add(0);
+        Track1.Add(0);
+        Track1.Add(0); //
+        Track1.Add(9999);
+        timeUntilBeat = 60 / BPMinute / BPMeasure;
+    }
+
+    void FixedUpdate()
+    {
+        timer = timer + Time.deltaTime;
+        if (timer >= timeUntilBeat)
+        {
+            timer = timer - timeUntilBeat;
+            beat++;
+        }
+        //print(beat + ", " + timer + "/" + timeUntilBeat);
+    }
+}
---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    private float originRotate;
+    private int noteRangeMin = 13;
+    private int noteRangeMax = 49;
+    private float angleRangeMin = -75;
+    private float angleRangeMax = 75;
+    private int currentNote;
+    private float noteRange;
+    private float angleRange;
+    private float finalRange;
+    private float degreesToRotate;
+    private bool done;
+    private float smooth = 30.0f;
+    private Quaternion target;
+    public TextMesh text;
+    private int currentNoteValue;
+    private int octave;
+    private string noteDisplay;
+    public GameObject laser;
+    private Vector3 laserActive;
+    private Vector3 laserInactive;
+
+    void Start()
+    {
+        originRotate = transform.rotation.z;
+        noteRange = noteRangeMax - noteRangeMin;
+        angleRange = angleRangeMax + Mathf.Abs(angleRangeMin);
+        finalRange = angleRange / noteRange;
+        laserActive = new Vector3(0.25f, 5, 0.25f);
+        laserInactive = new Vector3(0, 0, 0);
+    }
+
+    void FixedUpdate()
+    {
+        if (currentNote == 9999)
+        {
+            done = true;
+        }
+        if (GlobalNoteTracker.beat != 0 && !done)
+        {
+            currentNote = GlobalNoteTracker.Track1[GlobalNoteTracker.beat - 1];
+            smooth = 30f;
+            laser.transform.localScale = laserActive;
+        }
+        if (GlobalNoteTracker.beat == 0 || currentNote == 0)
+        {
+            target = Quaternion.Euler(0, 180, originRotate);
+            text.text = "Not playing";
+            smooth = 10f;
+            laser.transform.localScale = laserInactive;
+        }
+        else if (done)
+        {
+            target = Quaternion.Euler(0, 180, originRotate);
+            text.text = "Not playing";
+            smooth = 10f;
+            laser.transform.localScale = laserInactive;
+        }
+        else
+        {
+            if (currentNote != 9999)
+            {
+                degreesToRotate = angleRangeMin + (finalRange * (currentNote - noteRangeMin));
+                target = Quaternion.Euler(0, 180, degreesToRotate);
+            }
+        }
+        transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
+
+        if (!done)
+        {
+            currentNoteValue = currentNote;
+            octave = 0;
+            while (currentNoteValue > 12)
+            {
+                currentNoteValue = currentNoteValue - 12;
+                octave++;
+            }
+            switch (currentNoteValue)
+            {
+                case 1:
+                    noteDisplay = "C" + octave;
+                    break;
+                case 2:
+                    noteDisplay = "C#" + octave;
+                    break;
+                case 3:
+                    noteDisplay = "D" + octave;
+                    break;
+                case 4:
+                    noteDisplay = "D#" + octave;
+                    break;
+                case 5:
+                    noteDisplay = "E" + octave;
+                    break;
+                case 6:
+                    noteDisplay = "F" + octave;
+                    break;
+                case 7:
+                    noteDisplay = "F#" + octave;
+                    break;
+                case 8:
+                    noteDisplay = "G" + octave;
+                    break;
+                case 9:
+                    noteDisplay = "G#" + octave;
+                    break;
+                case 10:
+                    noteDisplay = "A" + octave;
+                    break;
+                case 11:
+                    noteDisplay = "A#" + octave;
+                    break;
+                case 12:
+                    noteDisplay = "B" + octave;
+                    break;
+            }
+            if (currentNote != 0)
+            {
+                text.text = noteDisplay;
+            }
+        }
+    }
+}
