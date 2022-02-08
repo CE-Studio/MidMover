6a06048386038a7debd4383245baf275bc5a3a66
clarence112 <clarencepacelli@gmail.com>
2022-02-08 15:35:29 -0500

    no message

--- Assets/Scripts/settingsStorage.cs
+++ Assets/Scripts/settingsStorage.cs
@@ -1,25 +1,39 @@
+using System;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class settingsStorage : MonoBehaviour
-{
+public class settingsStorage:MonoBehaviour {
+
+    [Serializable]
     public struct settingsGlobal {
-        public string test;
+        public Vector2 playbarPos;
+        public bool playbarShape;
+        public Vector2 titlebarPos;
+        public bool titlebarShape;
+        public float camPanScale;
+        public float camTransScale;
     }
 
-    public settingsGlobal globalSettings;
+    public static settingsGlobal globalSettings;
 
+    void Awake() {
+        if (File.Exists("Settings.json")) {
+            string savedata = File.ReadAllText("Settings.json");
+            globalSettings = JsonUtility.FromJson<settingsGlobal>(savedata);
+        } else {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+    }
+
+    void OnApplicationQuit() {
+        string savedata = JsonUtility.ToJson(globalSettings);
+        File.WriteAllText("Settings.json", savedata);
     }
 }
